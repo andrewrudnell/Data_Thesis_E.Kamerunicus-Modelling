acc5cde81aeea47d113ba57c2f5ad912c16aef6d
andrewrudnell <andrew16001@mail.unpad.ac.id>
2021-03-19 14:24:53 +0700

    Third Commit

--- Modelling Ecology Data E. kameruncius.R
+++ Modelling Ecology Data E. kameruncius.R
@@ -11,27 +11,27 @@ test.data <- data[-training.samples, ]
 
 #Modelling a data
 lmpol<- lm(Total.E.Kamerunicus ~ poly(Male.Flower, 4, raw = TRUE), data = train.data)
-glm<- glm(Total.E.Kamerunicus ~ Male.Flower, family = "poisson", data= train.data
-          gam <- gam(Total.E.Kamerunicus ~ s(Male.Flower), data =train.data)
-          summary(lmpol)
-          summary(glm)
-          predictions_glm <- glm %>% predict(test.data)
-          predictions <- lmpol %>% predict(test.data)
+glm<- glm(Total.E.Kamerunicus ~ Male.Flower, family = "poisson", data= train.data)
+gam <- gam(Total.E.Kamerunicus ~ s(Male.Flower), data =train.data)
+summary(lmpol)
+summary(glm)
+predictions_glm <- glm %>% predict(test.data)
+predictions <- lmpol %>% predict(test.data)
           
-          #Cross Validation
-          data.frame(
-            RMSE = RMSE(predictions_glm, test.data$Total.E.Kamerunicus),
-            R2 = R2(predictions_glm, test.data$Total.E.Kamerunicus)
-          )
-          data.frame(
-            RMSE = RMSE(predictions, test.data$Total.E.Kamerunicus),
-            R2 = R2(predictions, test.data$Total.E.Kamerunicus)
-          )
+#Cross Validation
+data.frame(
+  RMSE = RMSE(predictions_glm, test.data$Total.E.Kamerunicus),
+  R2 = R2(predictions_glm, test.data$Total.E.Kamerunicus)
+)
+data.frame(
+  RMSE = RMSE(predictions, test.data$Total.E.Kamerunicus),
+  R2 = R2(predictions, test.data$Total.E.Kamerunicus)
+)
           
-          #Visualize a model
-          ggplot(train.data, aes(Male.Flower, Total.E.Kamerunicus) ) +
-            labs(x ="Male Flower ", y = "E.Kameruncicus") + 
-            ggtitle("Relationship Between Male Flower & Population of E. Kamerunicus") +
-            geom_smooth(method = lm, formula = y ~ x, family =poisson(link = "log")) +
-            theme(plot.title = element_text(hjust = 0.5))
+#Visualize a model
+ggplot(train.data, aes(Male.Flower, Total.E.Kamerunicus) ) +
+  labs(x ="Male Flower ", y = "E.Kameruncicus") + 
+  ggtitle("Relationship Between Male Flower & Population of E. Kamerunicus") +
+  geom_smooth(method = lm, formula = y ~ x, family =poisson(link = "log")) +
+  theme(plot.title = element_text(hjust = 0.5))
           
